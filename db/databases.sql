05884ddb6bbf477d70aecfb6b97cf7e6f7198f10
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-03 20:31:27 -0400

    create databases

---
+++ db/databases.sql
@@ -0,0 +1,20 @@
+IF DB_ID('GMRustyHearts') IS NOT NULL
+  CREATE DATABASE GMRustyHearts;
+  GO
+
+IF DB_ID('RustyHearts') IS NOT NULL
+  CREATE DATABASE RustyHearts;
+  GO
+
+IF DB_ID('RustyHearts_Account') IS NOT NULL
+  CREATE DATABASE RustyHearts_Account;
+  GO
+
+IF DB_ID('RustyHearts_Auth') IS NOT NULL
+  CREATE DATABASE RustyHearts_Auth;
+  GO
+
+IF DB_ID('RustyHearts_Log') IS NOT NULL
+  CREATE DATABASE RustyHearts_Log;
+  GO
+

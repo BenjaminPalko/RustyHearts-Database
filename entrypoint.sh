3ae26607b547229376491b649ee66b185ba5bcaf
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-03 22:08:22 -0400

    USE THE CORRECT DATABASES!!![13;2u[13;2u

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ PASSWORD=$MSSQL_SA_PASSWORD
 /opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts -i ./db/RustyHearts.sql
 /opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Account -i ./db/RustyHearts_Account.sql
 /opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth_RealTimeEventTable -i ./db/RustyHearts_Auth_RealTimeEventTable.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth_RealTimeEventTable.sql
 /opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Log -i ./db/RustyHearts_Log.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Procedures -i ./db/RustyHearts_Procedures Fix.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts -i ./db/RustyHearts_Procedures_Fix.sql
 /opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/setup_auth.sql

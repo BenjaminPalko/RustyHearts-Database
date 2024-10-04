ac662c4e33b85137bf61ece1bbb3ebe04f56a52d
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-03 22:08:06 -0400

    holy fuck I cant write SQL

--- db/setup_auth.sql
+++ db/setup_auth.sql
@@ -1,7 +1,3 @@
-USE [RustyHearts_Auth]
-GO
-
 UPDATE ServerOption
-SET PublicAddress = "<SERVER_PUBLIC_IP>"
-WHERE PublicAddress = "192.168.1.100";
-GO
+SET PublicAddress = '192.168.100.3'
+WHERE PublicAddress = '<SERVER_PUBLIC_IP>';

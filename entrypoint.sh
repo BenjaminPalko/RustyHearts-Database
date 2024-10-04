d29538bef72be6e50d48175bd9dbe14cea032fa5
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-03 20:32:07 -0400

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-/opt/mssql/bin/sqlservr &
-(
-	sleep 10 &&
-		/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -i databases.sql &&
-		/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -i setup_auth.sql
-)
-BACK_PID=$!
-wait "$BACK_PID"
+HOST=$MSSQL_HOST
+PASSWORD=$MSSQL_SA_PASSWORD
+
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -i ./db/databases.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d GMRustyHearts -i ./db/GMRustyHearts.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts -i ./db/RustyHearts.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Account -i ./db/RustyHearts_Account.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth_RealTimeEventTable -i ./db/RustyHearts_Auth_RealTimeEventTable.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Log -i ./db/RustyHearts_Log.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Procedures -i ./db/RustyHearts_Procedures Fix.sql
+/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/setup_auth.sql

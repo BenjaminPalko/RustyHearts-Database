989b5a311431c73fd7613ec7ee7d904df77e5718
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-12 18:05:27 -0400

    updatooo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-HOST=$MSSQL_HOST
-PASSWORD=$MSSQL_SA_PASSWORD
+RUN sed -i "s/<SERVER_PUBLIC_IP>/${SERVER_PUBLIC_IP}/g" ./db/setup_auth.sql
 
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -i ./db/databases.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d GMRustyHearts -i ./db/GMRustyHearts.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts -i ./db/RustyHearts.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Account -i ./db/RustyHearts_Account.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth_RealTimeEventTable.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Log -i ./db/RustyHearts_Log.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts -i ./db/RustyHearts_Procedures_Fix.sql
-/opt/mssql-tools/bin/sqlcmd -S "$HOST" -U sa -P "$PASSWORD" -d RustyHearts_Auth -i ./db/setup_auth.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -i ./db/databases.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d GMRustyHearts -i ./db/GMRustyHearts.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts -i ./db/RustyHearts.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts -i ./db/RustyHearts_Procedures_Fix.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts_Account -i ./db/RustyHearts_Account.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts_Auth -i ./db/RustyHearts_Auth_RealTimeEventTable.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts_Log -i ./db/RustyHearts_Log.sql
+/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U sa -P "$MSSQL_SA_PASSWORD" -d RustyHearts_Auth -i ./db/setup_auth.sql
